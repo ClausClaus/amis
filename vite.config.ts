d30cb1b532440e38e3462e67b61daa39e24fb073
ClausClaus <weixinla84@gmail.com>
2023-01-20 18:46:37 +0800

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import mockApi from './scripts/mockApiPlugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     fis3(),
     markdown(),
@@ -41,7 +42,8 @@ export default defineConfig({
     }
   },
   server: {
-    port: 8888
+    port: 8888,
+    host: true
   },
   resolve: {
     alias: [
